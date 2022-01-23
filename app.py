55a3a634ea0aa2c3795b98300cb67653195e7e62
makitsukasa <allium.fistulosum.39@gmail.com>
2022-01-23 16:08:22 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import datetime
+from flask import Flask, request
+from flask_httpauth import HTTPDigestAuth
+
+app = Flask(__name__)
+auth = HTTPDigestAuth()
+
+users = {
+	"user01" : "pass01"
+}
+
+@app.route("/")
+@auth.login_required
+def app_route_index():
+	try:
+		return "Hello, World!"
+	except Exception as e:
+		return "Exception:" + str(traceback.format_exc()), 500
+		
+@auth.get_password
+def get_password(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+if __name__ == "__main__":
+	app.run(debug = True)
