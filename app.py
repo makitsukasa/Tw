82340d0ddd48674d2b4ccd0479a93734750eee3e
makitsukasa <allium.fistulosum.39@gmail.com>
2022-01-25 22:00:30 +0900

    single quotation

--- app.py
+++ app.py
@@ -12,36 +12,36 @@ Talisman(app)
 users = json.loads(os.getenv('CUSTOMCONNSTR_USERS'))
 auth = HTTPDigestAuth()
 
-@app.route("/")
+@app.route('/')
 @auth.login_required
 def app_route_index():
 	try:
-		return "Hello, " + auth.username()
+		return 'Hello, ' + auth.username()
 	except Exception as e:
-		return "Exception:" + str(traceback.format_exc()), 500
+		return 'Exception:' + str(traceback.format_exc()), 500
 
-@app.route("/tw")
+@app.route('/tw')
 @auth.login_required
 def app_route_tw():
 	return render_template('tw.html')
 
-@app.route("/tl")
+@app.route('/tl')
 @auth.login_required
 def app_route_tl():
 	return render_template('tl.html', home_timeline = home_timeline())
 
-@app.route("/update_status", methods=["POST"])
+@app.route('/update_status', methods=['POST'])
 @auth.login_required
 def app_route_update_status():
-	body = request.form.get("body")
+	body = request.form.get('body')
 	if not update_status(body):
-		return "/update_status server error", 500
-	return "/update_status post succeeded"
+		return '/update_status server error', 500
+	return '/update_status post succeeded'
 
-@app.route("/receive", methods=["GET", "POST"])
+@app.route('/receive', methods=['GET', 'POST'])
 def app_route_receive():
-	return "HRADER<br>" + str(request.headers) + "<br><br>" +\
-		"DATA<br>" + request.get_data(as_text=True)
+	return 'HRADER<br>' + str(request.headers) + '<br><br>' +\
+		'DATA<br>' + request.get_data(as_text=True)
 
 @auth.get_password
 def get_password(username):
@@ -49,5 +49,5 @@ def get_password(username):
 		return users.get(username)
 	return None
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app.run(debug=True)
