15a7e36c02ac1b9624b93bf138103b9bb59d8a99
makitsukasa <allium.fistulosum.39@gmail.com>
2022-02-03 17:11:15 +0900

    消し忘れ

--- static/js/tl.js
+++ static/js/tl.js
@@ -19,16 +19,6 @@ function retweet(e) {
 	return false;
 }
 
-function showImage(e) {
-	e.preventDefault();
-	const id = e.target.parentElement.parentElement.id;
-	const request = new XMLHttpRequest();
-	request.onload = () => {};
-	request.open('post', '/show_image/' + id, true);
-	request.send();
-	return false;
-}
-
 window.addEventListener('load', function(){
 	const favButtons = document.getElementsByClassName('fav_button');
 	for(let i = 0; i < favButtons.length; i++) {
@@ -38,10 +28,5 @@ window.addEventListener('load', function(){
 	const rtButtons = document.getElementsByClassName('rt_button');
 	for(let i = 0; i < rtButtons.length; i++) {
 		rtButtons[i].addEventListener('click', retweet, false);
-	}
-
-	const imgButtons = document.getElementsByClassName('img_button');
-	for(let i = 0; i < imgButtons.length; i++) {
-		imgButtons[i].addEventListener('click', showImage, false);
 	}
 }, false);
