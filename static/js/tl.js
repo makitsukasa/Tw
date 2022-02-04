0b932706ee3a9e1720564d9170ba3611fa8446af
makitsukasa <allium.fistulosum.39@gmail.com>
2022-02-04 10:39:44 +0900

    unfav button

--- static/js/tl.js
+++ static/js/tl.js
@@ -3,8 +3,18 @@ function createFavorite(e) {
 	e.preventDefault();
 	const id = e.target.parentElement.parentElement.id;
 	const request = new XMLHttpRequest();
-	request.onload = () => {};
+	request.onload = () => {toggleFavButton(id)};
 	request.open('post', '/create_favorite/' + id, true);
+	request.send();
+	return false;
+}
+
+function destroyFavorite(e) {
+	e.preventDefault();
+	const id = e.target.parentElement.parentElement.id;
+	const request = new XMLHttpRequest();
+	request.onload = () => {toggleFavButton(id)};
+	request.open('post', '/destroy_favorite/' + id, true);
 	request.send();
 	return false;
 }
@@ -19,10 +29,22 @@ function retweet(e) {
 	return false;
 }
 
+function toggleFavButton(id) {
+	const favButton = document.querySelector(`#{id} > .buttons > .fav_button`);
+	const unfavButton = document.querySelector(`#{id} > .buttons > .unfav_button`);
+	favButton.disabled = !favButton.disabled
+	unfavButton.disabled = !unfavButton.disabled
+}
+
 window.addEventListener('load', function(){
 	const favButtons = document.getElementsByClassName('fav_button');
 	for(let i = 0; i < favButtons.length; i++) {
 		favButtons[i].addEventListener('click', createFavorite, false);
+	}
+
+	const unfavButtons = document.getElementsByClassName('unfav_button');
+	for(let i = 0; i < unfavButtons.length; i++) {
+		unfavButtons[i].addEventListener('click', destroyFavorite, false);
 	}
 
 	const rtButtons = document.getElementsByClassName('rt_button');
