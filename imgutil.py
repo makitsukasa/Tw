fcdc65039c210a0f2d6a7cfd1433a08b59d817b3
makitsukasa <allium.fistulosum.39@gmail.com>
2022-02-07 05:13:53 +0900

    pngのほうがいいね

--- imgutil.py
+++ imgutil.py
@@ -5,13 +5,9 @@ import base64
 
 # https://teratail.com/questions/89341
 def base64ify(img_url):
-	print(img_url)
 	response = requests.get(img_url)
 	img = Image.open(BytesIO(response.content))
-	if img.mode != "RGB": # http://blog.livedoor.jp/kmiwa_project/archives/1042268915.html
-		img = img.convert("RGB")
-
 	buffer = BytesIO()
-	img.save(buffer, 'jpeg')
+	img.save(buffer, 'png')
 	base64string = base64.b64encode(buffer.getvalue())
-	return 'data:image/jpg;base64,' + base64string.decode('utf-8')
+	return 'data:image/png;base64,' + base64string.decode('utf-8')
