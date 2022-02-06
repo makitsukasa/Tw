a11a897c423c973bd520fa70cc50aea962f37520
makitsukasa <allium.fistulosum.39@gmail.com>
2022-02-07 04:38:16 +0900

    なんかだめみたいなんすよね

--- imgutil.py
+++ imgutil.py
@@ -5,10 +5,11 @@ import base64
 
 # https://teratail.com/questions/89341
 def base64ify(img_url):
+	print(img_url)
 	response = requests.get(img_url)
 	img = Image.open(BytesIO(response.content))
 
 	buffer = BytesIO()
 	img.save(buffer, 'jpg')
 	base64string = base64.b64encode(buffer.getvalue())
-	return 'data:image/png;base64,' + base64string.decode('utf-8')
+	return 'data:image/jpg;base64,' + base64string.decode('utf-8')
