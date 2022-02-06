64053a8e7a5ab9ebce175a67bfc54695a603a62e
makitsukasa <allium.fistulosum.39@gmail.com>
2022-02-07 05:10:46 +0900

    こっちでした

--- imgutil.py
+++ imgutil.py
@@ -8,10 +8,10 @@ def base64ify(img_url):
 	print(img_url)
 	response = requests.get(img_url)
 	img = Image.open(BytesIO(response.content))
+	if img.mode != "RGB": # http://blog.livedoor.jp/kmiwa_project/archives/1042268915.html
+		img = img.convert("RGB")
 
 	buffer = BytesIO()
 	img.save(buffer, 'jpeg')
-	if img.mode != "RGB": # http://blog.livedoor.jp/kmiwa_project/archives/1042268915.html
-		img = img.convert("RGB")
 	base64string = base64.b64encode(buffer.getvalue())
 	return 'data:image/jpg;base64,' + base64string.decode('utf-8')
